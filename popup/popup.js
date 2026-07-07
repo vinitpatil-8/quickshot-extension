1d32cbe51eac936966439492bd2da1d58b2a02bc
Vinit <vinit.2007.20@gmail.com>
2026-07-07 22:37:58 +0530

    Enhance popup UI with CSS variables and light mode support
    - Refactored popup.css to use CSS variables for colors and styles, improving maintainability.
    - Updated popup.html to include a new header section with an icon and settings button.
    - Replaced static image buttons with SVG icons for better scalability and performance.
    - Implemented light mode functionality in popup.js, adjusting styles based on system color scheme preferences.

--- popup/popup.js
+++ popup/popup.js
@@ -12,3 +12,22 @@ document.querySelector("#screenshotBtn").addEventListener("click", async () => {
         console.error("Failed to start the screenshot tool.", error);
     }
 });
+
+// UI
+
+var r = document.querySelector(':root');
+
+function light_mode(){
+    r.style.setProperty('--primary-bg', '#f6fcfc');
+    r.style.setProperty('--primary-text', '#000');
+    r.style.setProperty('--primary-tooltip', '#eeedee');
+    r.style.setProperty('--primary-shadow', 'rgba(255, 255, 255, 0.25)');
+}
+
+const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (isDarkMode) {
+    console.log("System is in Dark Mode");
+} else {
+    light_mode()
+}
